a43bd1107054fe652165a812de4717cbb14cf14b
Veronika <griva.verona@gmail.com>
2022-10-03 02:33:48 +0300

    add function

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,16 @@
 ﻿Console.WriteLine("\nПервоначальный массив:");
 string[] array1 = {"Minsk", ";-)", "22", "opa", "56473", "GB", "S_N", "Romeo"};
 string[] array2 = new string[array1.Length];
+ResultArray(array1, array2);
+void ResultArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+            {
+            array2[count] = array1[i];
+            count++;
+            }
+    }
+}
